e45923df384ccbbd782aa4857a1f43bf0493ac72
Mat Sletten <matsletten@mmintzmbp.bbbb.net>
2014-03-24 16:05:36 -0700

    Initial Commit

---
+++ Laika/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  Laika
+//
+//  Created by Mat Sletten on 3/24/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Laika/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Laika
+//
+//  Created by Mat Sletten on 3/24/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
